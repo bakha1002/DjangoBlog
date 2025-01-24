9fad630c0556977a574545bc67bce9ff3a0b0ed6
bakha1002 <yh795483@naver.com>
2025-01-24 14:12:37 +0900

    [feat] django hello world, random

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import *
+
 
 urlpatterns = [
+    path("hello/", helloworld),
+    path("hello/json/", helloworld_json),
+    path("random/", random_number),
     path('admin/', admin.site.urls),
 ]
